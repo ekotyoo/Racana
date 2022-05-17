2c6fdc6110a5d4808790a430065e2565c2886a9e
ekotyoo <ekeprasetyo@gmail.com>
2022-05-17 14:04:34 +0700

    Changing status bar color

--- app/src/main/java/com/ekotyoo/racana/core/theme/Theme.kt
+++ app/src/main/java/com/ekotyoo/racana/core/theme/Theme.kt
@@ -5,6 +5,10 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import timber.log.Timber
 
 private val DarkColorPalette = darkColors(
     primary = ROrange,
@@ -22,6 +26,16 @@ private val LightColorPalette = lightColors(
 
 @Composable
 fun RacanaTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    val systemUiController = rememberSystemUiController()
+    val useDarkIcons = MaterialTheme.colors.isLight
+
+    SideEffect {
+        systemUiController.setStatusBarColor(
+            color = Color.Transparent,
+            darkIcons = useDarkIcons
+        )
+    }
+
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
