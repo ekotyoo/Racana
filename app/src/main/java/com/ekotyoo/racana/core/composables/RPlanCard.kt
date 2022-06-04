5121582fbc1f329a862288fc9860791b5778d27f
ekotyoo <ekeprasetyo@gmail.com>
2022-06-04 20:00:55 +0700

    Adding card options menu

--- app/src/main/java/com/ekotyoo/racana/core/composables/RPlanCard.kt
+++ app/src/main/java/com/ekotyoo/racana/core/composables/RPlanCard.kt
@@ -3,17 +3,18 @@ package com.ekotyoo.racana.core.composables
 import android.content.res.Configuration
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.material.icons.rounded.MoreVert
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import com.ekotyoo.racana.R
 import com.ekotyoo.racana.core.theme.RacanaTheme
@@ -27,54 +28,74 @@ fun RPlanCard(
     description: String,
     imageUrl: String,
     onClick: () -> Unit,
-    onDelete: () -> Unit
+    onDelete: () -> Unit,
 ) {
+    var popupMenuVisible by remember { mutableStateOf(false) }
+
     Card(
         modifier = modifier
             .height(116.dp)
             .fillMaxWidth(),
         elevation = 8.dp,
     ) {
-        Row(
-            modifier = Modifier
-                .clickable(onClick = onClick)
-                .padding(8.dp)
-        ) {
-            CoilImage(
+        Box {
+            Row(
                 modifier = Modifier
-                    .clip(MaterialTheme.shapes.medium)
-                    .fillMaxHeight()
-                    .aspectRatio(1f),
-                imageModel = imageUrl,
-                contentScale = ContentScale.Crop,
-                previewPlaceholder = R.drawable.ic_launcher_background,
-                contentDescription = null
-            )
-            Spacer(modifier = Modifier.width(8.dp))
-            Column(modifier = Modifier.weight(1f)) {
-                Text(
-                    text = name,
-                    style = MaterialTheme.typography.subtitle2,
-                    modifier = Modifier.padding(top = 6.dp)
+                    .clickable(onClick = onClick)
+                    .padding(8.dp)
+            ) {
+                CoilImage(
+                    modifier = Modifier
+                        .clip(MaterialTheme.shapes.medium)
+                        .fillMaxHeight()
+                        .aspectRatio(1f),
+                    imageModel = imageUrl,
+                    contentScale = ContentScale.Crop,
+                    previewPlaceholder = R.drawable.ic_launcher_background,
+                    contentDescription = null
                 )
-                Text(
-                    text = date,
-                    style = MaterialTheme.typography.body2
-                )
-                Spacer(modifier = Modifier.height(5.dp))
-                Text(
-                    text = description,
-                    style = MaterialTheme.typography.caption
-                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Column(modifier = Modifier.weight(1f)) {
+                    Text(
+                        text = name,
+                        style = MaterialTheme.typography.subtitle1,
+                    )
+                    Text(
+                        text = date,
+                        style = MaterialTheme.typography.body1
+                    )
+                    Text(
+                        text = description,
+                        style = MaterialTheme.typography.body2
+                    )
+                }
+                IconButton(onClick = {
+                    popupMenuVisible = true
+                }) {
+                    Icon(
+                        imageVector = Icons.Rounded.MoreVert,
+                        contentDescription = stringResource(id = R.string.item_option_button)
+                    )
+                }
             }
-            RIconButton(
-                imageVector = Icons.Default.Delete,
-                contentDescription = null,
-                onClick = onDelete,
-                background = MaterialTheme.colors.surface,
-                tint = MaterialTheme.colors.error,
-                modifier = Modifier.align(CenterVertically)
-            )
+            val configuration = LocalConfiguration.current
+            val offsetX = configuration.screenWidthDp.dp
+            DropdownMenu(
+                expanded = popupMenuVisible,
+                offset = DpOffset(x = offsetX, y = (-116).dp),
+                onDismissRequest = {
+                    popupMenuVisible = false
+                }
+            ) {
+                DropdownMenuItem(onClick = {
+                    onDelete()
+                }) {
+                    Text(
+                        text = stringResource(id = R.string.delete_tour_plan),
+                        style = MaterialTheme.typography.body1
+                    )
+                }
+            }
         }
     }
 }
