57a1963f28f98fb808bd2b6d6a0ef61c24e64ee1
ekotyoo <ekeprasetyo@gmail.com>
2022-06-08 17:22:04 +0700

    add dataset rating endpoint

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const db = new sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: "mysql",
-  dialectOptions: { decimalNumbers: true, socketPath: process.env.DB_HOST },
+  dialectOptions: { decimalNumbers: true },
 });
 
 module.exports = db;
